e79fd2055d0f968e518b82e832ad9849fd1ee9e0
Ashok <deb.ashok2019@gmail.com>
2025-02-13 22:33:37 +0530

    Spring tutorial

---
+++ src/test/java/com/ashok/test/springIntro/SpringIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashok.test.springIntro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIntroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
