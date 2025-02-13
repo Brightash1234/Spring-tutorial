7e4ebde95a7304b826426a6778e3e495af8f7167
Ashok <deb.ashok2019@gmail.com>
2025-02-13 23:15:40 +0530

    Changes

---
+++ src/main/java/com/ashok/test/springIntro/Apple.java
@@ -0,0 +1,11 @@
+package com.ashok.test.springIntro;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Apple {
+    void eatApple() {
+        System.out.println("I am eating apple");
+    }
+
+}
--- src/main/java/com/ashok/test/springIntro/SpringIntroApplication.java
+++ src/main/java/com/ashok/test/springIntro/SpringIntroApplication.java
@@ -1,13 +1,21 @@
 package com.ashok.test.springIntro;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringIntroApplication {
+public class SpringIntroApplication implements CommandLineRunner {
+	@Autowired
+	Apple obj;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringIntroApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+			obj.eatApple();
+	}
 }
